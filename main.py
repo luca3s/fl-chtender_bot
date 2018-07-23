b2cdc94dd13cbd50a580620d91e8d4cd4be03e9b
luca3s <41015240+luca3s@users.noreply.github.com>
2018-07-23 14:42:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ TOKEN = os.environ['TOKEN']
 
 client = discord.Client()
 
-channel_id = '466665501247012874'
+fortnite-infos_id = '466665501247012874'
+regeln_id = '466671498149625887'
+infos_id = '467369390086815755'
 role_1 = 'Mod'
 role_2 = 'Ober Wolf'
 
@@ -19,14 +21,21 @@ async def on_ready():
     
 @client.event
 async def on_message(message):
-    channel = client.get_channel(channel_id)
-
+    fortnite-infos = client.get_channel(fortnite-infos_id)
+    regeln = client.get_channel(regeln_id)
+    infos = client.get_channel(infos_id)
+    
     if message.channel == channel:
         if message.author == client.user or message.author.top_role.name == role_1 or message.author.top_role.name == role_2:
             return
         else:
             warning = 'Wer in diesen Channel schreibt wird gebannt vergisst es nicht. Es wurde schon einer dafür gebannt'.format(message)
-            await client.send_message(message.channel, warning)
+
+            await client.send_message(fortnite-infos, warning)
+            await client.send_message(infos, warning)
+            await client.send_message(regeln, warning)
+            
             await client.ban(message.author)
+            await client.delete_message(message)
             
 client.run(TOKEN)
